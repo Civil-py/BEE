472a6dc2291f7b153ad992aa008c240b8948a0fd
Civil-py <civilmotha@gmail.com>
2024-06-24 23:39:27 +0200

    Update settings.py

--- p/p/settings.py
+++ p/p/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'bee/static',
+    BASE_DIR / 'bee/static/bee',
 ]
 
 # Default primary key field type
