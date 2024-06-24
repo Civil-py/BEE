8c0d51bd994bb47bf1bc29d7f293a28a119abcde
Civil-py <civilmotha@gmail.com>
2024-06-24 23:24:04 +0200

    Update settings.py

--- p/p/settings.py
+++ p/p/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'bee/static',
 ]
-
-
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
