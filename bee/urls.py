f8431928f1b26c677c4bd4460ed8929816999489
Civil-py <civilmotha@gmail.com>
2024-08-13 14:20:28 +0200

    Update urls.py

--- bee/urls.py
+++ bee/urls.py
@@ -8,10 +8,10 @@ path("", views.landingpage, name="landingpage"),
 path("register", views.register, name="register"),
 
 
-    path('cognito/callback/', views.cognito_callback, name='cognito_callback'),
+    path('cognito/callback', views.cognito_callback, name='cognito_callback'),
 path('user-info/<str:username>/', views.get_cognito_user_info, name='user-info'),
 
-    path('bee/home/', views.login_view, name='login'),
+    path('bee/home', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 
 path("home/Inputs/<str:choice>", views.inputs, name="inputs"),
